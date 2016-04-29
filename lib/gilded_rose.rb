882846710dd7902dd644a5b45f1c86220d42a453
Ina <inatsetsova@gmail.com>
2016-04-29 10:22:43 +0100

    spaces code out a bit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -15,12 +15,14 @@ class GildedRose
       else
         if item.quality < 50
           item.quality = item.quality + 1
+
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
               end
             end
+
             if item.sell_in < 6
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,9 +31,11 @@ class GildedRose
           end
         end
       end
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -53,16 +57,3 @@ class GildedRose
   end
 end
 
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
-end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,13 @@
-require File.join(File.dirname(__FILE__), 'gilded_rose')
+require 'gilded_rose'
+require 'spec_helper'
 
 describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      items[0].name.should == "fixme"
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq("fixme")
     end
   end
 
